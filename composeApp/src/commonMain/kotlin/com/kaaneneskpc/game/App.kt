5692d6f885e2441819b137d5b342673f890ac050
kaaneneskpc <kaaneneskpc1@gmail.com>
2025-01-19 02:57:37 +0300

    - Added pipes and background moving scene

--- composeApp/src/commonMain/kotlin/com/kaaneneskpc/game/App.kt
+++ composeApp/src/commonMain/kotlin/com/kaaneneskpc/game/App.kt
@@ -1,7 +1,12 @@
 package com.kaaneneskpc.game
 
 
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
 import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -14,6 +19,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -32,8 +38,10 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.rotate
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.kaaneneskpc.game.domain.Game
@@ -46,25 +54,30 @@ import com.stevdza_san.sprite.domain.rememberSpriteState
 import gamingcmp.composeapp.generated.resources.Res
 import gamingcmp.composeapp.generated.resources.background
 import gamingcmp.composeapp.generated.resources.bee_sprite
+import gamingcmp.composeapp.generated.resources.moving_background
+import gamingcmp.composeapp.generated.resources.pipe
+import gamingcmp.composeapp.generated.resources.pipe_cap
+import org.jetbrains.compose.resources.imageResource
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 const val BEE_FRAME_SIZE = 80
 const val TOTAL_FRAMES = 9
 const val FRAMES_PER_ROW = 3
+const val PIPE_CAP_HEIGHT = 50F
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
+        val platform = remember { getPlatform() }
         var screenWidth by remember { mutableStateOf(0) }
         var screenHeight by remember { mutableStateOf(0) }
-        var game by remember { mutableStateOf(Game()) }
+        var game by remember { mutableStateOf(Game(platform)) }
         val spriteState = rememberSpriteState(
             totalFrames = TOTAL_FRAMES,
-            framesPerRow = FRAMES_PER_ROW,
-
-            )
+            framesPerRow = FRAMES_PER_ROW
+        )
 
         val spriteSpec = remember {
             SpriteSpec(
@@ -113,134 +126,247 @@ fun App() {
             }
         }
 
-        Box(modifier = Modifier.fillMaxSize()) {
+        val backgroundOffsetX = remember { Animatable(0f) }
+        var imageWidth by remember { mutableStateOf(0) }
+
+        val pipeImage = imageResource(Res.drawable.pipe)
+        val pipeCapImage = imageResource(Res.drawable.pipe_cap)
+
+        LaunchedEffect(game.status) {
+            while (game.status == GameStatus.Started) {
+                backgroundOffsetX.animateTo(
+                    targetValue = -imageWidth.toFloat(),
+                    animationSpec = infiniteRepeatable(
+                        animation = tween(
+                            durationMillis = when(platform) {
+                                Platform.Android -> 4000
+                                Platform.iOS -> 4000
+                                Platform.Web -> 11000
+                                Platform.Desktop -> 9000
+                            },
+                            easing = LinearEasing
+                        ),
+                        repeatMode = RepeatMode.Restart
+                    )
+                )
+            }
+        }
+
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
             Image(
                 modifier = Modifier.fillMaxSize(),
                 painter = painterResource(Res.drawable.background),
                 contentDescription = "Background",
                 contentScale = ContentScale.Crop
             )
-
-            Canvas(
+            Image(
                 modifier = Modifier
                     .fillMaxSize()
-                    .onGloballyPositioned {
-                        val size = it.size
-                        if (screenWidth != size.width || screenHeight != size.height) {
-                            screenWidth = size.width
+                    .onSizeChanged {
+                        imageWidth = it.width
+                    }
+                    .offset {
+                        IntOffset(
+                            x = backgroundOffsetX.value.toInt(),
+                            y = 0
+                        )
+                    },
+                painter = painterResource(Res.drawable.moving_background),
+                contentDescription = "Moving Background",
+                contentScale = ContentScale.FillHeight
+            )
+            Image(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .offset {
+                        IntOffset(
+                            x = backgroundOffsetX.value.toInt() + imageWidth,
+                            y = 0
+                        )
+                    },
+                painter = painterResource(Res.drawable.moving_background),
+                contentDescription = "Moving Background",
+                contentScale = ContentScale.FillHeight
+            )
+        }
+
+        Canvas(
+            modifier = Modifier
+                .fillMaxSize()
+                .onGloballyPositioned {
+                    val size = it.size
+                    if (screenWidth != size.width || screenHeight != size.height) {
+                        screenWidth = size.width
+                        screenHeight = size.height
+                        game = game.copy(
+                            screenWidth = size.width,
                             screenHeight = size.height
-                            game = Game(screenWidth = screenWidth, screenHeight = screenHeight)
-                        }
-                    }
-                    .clickable {
-                        if (game.status == GameStatus.Started) {
-                            game.jump()
-                        }
-                    }
-            ) {
-                rotate(
-                    degrees = animatedAngle,
-                    pivot = Offset(
-                        x = game.bee.x - game.beeRadius,
-                        y = game.bee.y - game.beeRadius
-                    )
-                ) {
-                    drawSpriteView(
-                        spriteState = spriteState,
-                        spriteSpec = spriteSpec,
-                        currentFrame = currentFrame,
-                        image = sheetImage,
-                        offset = IntOffset(
-                            x = (game.bee.x - game.beeRadius).toInt(),
-                            y = (game.bee.y - game.beeRadius).toInt()
-                        )
-                    )
-                }
-            }
-
-            Row(
-                modifier = Modifier.fillMaxWidth().padding(all = 48.dp),
-                horizontalArrangement = Arrangement.SpaceBetween
-            ) {
-                Text(
-                    text = "BEST: 0",
-                    fontWeight = FontWeight.Bold,
-                    fontSize = MaterialTheme.typography.displaySmall.fontSize,
-                    fontFamily = ChewyFontFamily()
-                )
-                Text(
-                    text = "0",
-                    fontWeight = FontWeight.Bold,
-                    fontSize = MaterialTheme.typography.displaySmall.fontSize,
-                    fontFamily = ChewyFontFamily()
-                )
-            }
-
-            if (game.status == GameStatus.Idle) {
-                Box(
-                    modifier = Modifier.fillMaxSize().background(Color.Black.copy(alpha = 0.5f)),
-                    contentAlignment = Alignment.Center
-                ) {
-                    Button(
-                        modifier = Modifier.height(54.dp),
-                        shape = RoundedCornerShape(size = 20.dp),
-                        colors = ButtonDefaults.buttonColors(contentColor = Color.White),
-                        onClick = {
-                            game.start()
-                            spriteState.start()
-                        }
-                    ) {
-                        Icon(
-                            imageVector = Icons.Default.PlayArrow,
-                            contentDescription = null,
-                            tint = Color.White
-                        )
-                        Spacer(modifier = Modifier.width(6.dp))
-                        Text(
-                            text = "START",
-                            fontSize = MaterialTheme.typography.titleLarge.fontSize,
-                            fontFamily = ChewyFontFamily()
                         )
                     }
                 }
+                .clickable {
+                    if (game.status == GameStatus.Started) {
+                        game.jump()
+                    }
+                }
+        ) {
+            rotate(
+                degrees = animatedAngle,
+                pivot = Offset(
+                    x = game.bee.x - game.beeRadius,
+                    y = game.bee.y - game.beeRadius
+                )
+            ) {
+                drawSpriteView(
+                    spriteState = spriteState,
+                    spriteSpec = spriteSpec,
+                    currentFrame = currentFrame,
+                    image = sheetImage,
+                    offset = IntOffset(
+                        x = (game.bee.x - game.beeRadius).toInt(),
+                        y = (game.bee.y - game.beeRadius).toInt()
+                    )
+                )
             }
+            game.pipePairs.forEach { pipePair ->
+                drawImage(
+                    image = pipeImage,
+                    dstOffset = IntOffset(
+                        x = (pipePair.x - game.pipeWidth / 2).toInt(),
+                        y = 0
+                    ),
+                    dstSize = IntSize(
+                        width = game.pipeWidth.toInt(),
+                        height = (pipePair.topHeight - PIPE_CAP_HEIGHT).toInt()
+                    )
+                )
+                drawImage(
+                    image = pipeCapImage,
+                    dstOffset = IntOffset(
+                        x = (pipePair.x - game.pipeWidth / 2).toInt(),
+                        y = (pipePair.topHeight - PIPE_CAP_HEIGHT).toInt()
+                    ),
+                    dstSize = IntSize(
+                        width = game.pipeWidth.toInt(),
+                        height = PIPE_CAP_HEIGHT.toInt()
+                    )
+                )
+                drawImage(
+                    image = pipeCapImage,
+                    dstOffset = IntOffset(
+                        x = (pipePair.x - game.pipeWidth / 2).toInt(),
+                        y = (pipePair.y + game.pipeGapSize / 2).toInt()
+                    ),
+                    dstSize = IntSize(
+                        width = game.pipeWidth.toInt(),
+                        height = PIPE_CAP_HEIGHT.toInt()
+                    )
+                )
+                drawImage(
+                    image = pipeImage,
+                    dstOffset = IntOffset(
+                        x = (pipePair.x - game.pipeWidth / 2).toInt(),
+                        y = (pipePair.y + game.pipeGapSize / 2 + PIPE_CAP_HEIGHT).toInt()
+                    ),
+                    dstSize = IntSize(
+                        width = game.pipeWidth.toInt(),
+                        height = (pipePair.bottomHeight - PIPE_CAP_HEIGHT).toInt()
+                    )
+                )
+            }
+        }
 
+        Row(
+            modifier = Modifier.fillMaxWidth().padding(all = 48.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = "BEST: 0",
+                fontWeight = FontWeight.Bold,
+                fontSize = MaterialTheme.typography.displaySmall.fontSize,
+                fontFamily = ChewyFontFamily()
+            )
+            Text(
+                text = "0",
+                fontWeight = FontWeight.Bold,
+                fontSize = MaterialTheme.typography.displaySmall.fontSize,
+                fontFamily = ChewyFontFamily()
+            )
+        }
 
-            if (game.status == GameStatus.Over) {
-                Column(
-                    modifier = Modifier.fillMaxSize().background(Color.Black.copy(alpha = 0.5f)),
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally
+        if (game.status == GameStatus.Idle) {
+            Box(
+                modifier = Modifier.fillMaxSize().background(Color.Black.copy(alpha = 0.5f)),
+                contentAlignment = Alignment.Center
+            ) {
+                Button(
+                    modifier = Modifier.height(54.dp),
+                    shape = RoundedCornerShape(size = 20.dp),
+                    colors = ButtonDefaults.buttonColors(contentColor = Color.White),
+                    onClick = {
+                        game.start()
+                        spriteState.start()
+                    }
                 ) {
+                    Icon(
+                        imageVector = Icons.Default.PlayArrow,
+                        contentDescription = null,
+                        tint = Color.White
+                    )
+                    Spacer(modifier = Modifier.width(6.dp))
                     Text(
-                        text = "GAME OVER",
-                        color = Color.White,
-                        fontWeight = FontWeight.Bold,
-                        fontSize = MaterialTheme.typography.displayMedium.fontSize,
+                        text = "START",
+                        fontSize = MaterialTheme.typography.titleLarge.fontSize,
                         fontFamily = ChewyFontFamily()
                     )
-                    Spacer(modifier = Modifier.height(24.dp))
-                    Button(
-                        modifier = Modifier.height(54.dp),
-                        shape = RoundedCornerShape(size = 20.dp),
-                        colors = ButtonDefaults.buttonColors(contentColor = Color.White),
-                        onClick = {
-                            game.restart()
-                            spriteState.start()
-                        }
-                    ) {
-                        Icon(
-                            imageVector = Icons.Default.PlayArrow,
-                            contentDescription = null,
-                            tint = Color.White
-                        )
-                        Spacer(modifier = Modifier.width(6.dp))
-                        Text(
-                            text = "START",
-                            fontSize = MaterialTheme.typography.titleLarge.fontSize,
-                            fontFamily = ChewyFontFamily()
-                        )
+                }
+            }
+        }
+
+
+        if (game.status == GameStatus.Over) {
+            Column(
+                modifier = Modifier.fillMaxSize().background(Color.Black.copy(alpha = 0.5f)),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "GAME OVER",
+                    color = Color.White,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = MaterialTheme.typography.displayMedium.fontSize,
+                    fontFamily = ChewyFontFamily()
+                )
+                Text(
+                    text = "SCORE: ${game.currentScore}",
+                    color = Color.White,
+                    fontSize = MaterialTheme.typography.titleLarge.fontSize,
+                    fontFamily = ChewyFontFamily()
+                )
+                Spacer(modifier = Modifier.height(24.dp))
+                Button(
+                    modifier = Modifier.height(54.dp),
+                    shape = RoundedCornerShape(size = 20.dp),
+                    colors = ButtonDefaults.buttonColors(contentColor = Color.White),
+                    onClick = {
+                        game.restart()
+                        spriteState.start()
                     }
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.PlayArrow,
+                        contentDescription = null,
+                        tint = Color.White
+                    )
+                    Spacer(modifier = Modifier.width(6.dp))
+                    Text(
+                        text = "START",
+                        fontSize = MaterialTheme.typography.titleLarge.fontSize,
+                        fontFamily = ChewyFontFamily()
+                    )
                 }
             }
         }
--- composeApp/src/commonMain/kotlin/com/kaaneneskpc/game/domain/Game.kt
+++ composeApp/src/commonMain/kotlin/com/kaaneneskpc/game/domain/Game.kt
@@ -1,16 +1,25 @@
 package com.kaaneneskpc.game.domain
 
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import com.kaaneneskpc.game.Platform
+import kotlin.random.Random
+
+const val SCORE_KEY = "score"
 
 data class Game(
+    val platform: Platform,
     val screenWidth: Int = 0,
     val screenHeight: Int = 0,
-    val gravity: Float = 0.7f,
+    val gravity: Float = if (platform == Platform.Android) 0.8f else if (platform == Platform.iOS) 0.8f else 0.25f,
     val beeRadius: Float = 30f,
-    val beeJumpImpulse: Float = -12f,
-    val beeMaxVelocity: Float = 25f,
+    val beeJumpImpulse: Float = if (platform == Platform.Android) -12f else if (platform == Platform.iOS) -12f else -8f,
+    val beeMaxVelocity: Float = if (platform == Platform.Android) 25f else if(platform == Platform.iOS) 20f else 20f,
+    val pipeWidth: Float = 150f,
+    val pipeVelocity: Float = if (platform == Platform.Android) 5f else if(platform == Platform.iOS) 7f else 2.5f,
+    val pipeGapSize: Float = if (platform == Platform.Android) 250f else 300f
 ) {
     var status by mutableStateOf(GameStatus.Idle)
         private set
@@ -24,6 +33,13 @@ data class Game(
         )
     )
         private set
+
+    var pipePairs = mutableStateListOf<PipePair>()
+    var currentScore by mutableStateOf(0)
+        private set
+    var bestScore by mutableStateOf(0)
+        private set
+    private var isFallingSoundPlayed = false
 
     fun start() {
         status = GameStatus.Started
@@ -39,12 +55,17 @@ data class Game(
 
     fun restart() {
         resetBeePosition()
+        removePipes()
         start()
     }
 
     private fun resetBeePosition() {
         bee = bee.copy(y = (screenHeight / 2).toFloat())
         beeVelocity = 0f
+    }
+
+    private fun removePipes() {
+        pipePairs.clear()
     }
 
     fun updateGameProgress() {
@@ -58,7 +79,32 @@ data class Game(
 
         beeVelocity = (beeVelocity + gravity).coerceIn(-beeMaxVelocity, beeMaxVelocity)
         bee = bee.copy(y = bee.y + beeVelocity)
+
+        spawnPipes()
     }
+
+    private fun spawnPipes() {
+        pipePairs.forEach { it.x -= pipeVelocity }
+        pipePairs.removeAll { it.x + pipeWidth < 0 }
+
+        val isLandscape = screenWidth > screenHeight
+        val spawnThreshold = if (isLandscape) screenWidth / 1.25
+        else screenWidth / 2.0
+
+        if (pipePairs.isEmpty() || pipePairs.last().x < spawnThreshold) {
+            val initialPipeX = screenWidth.toFloat() + pipeWidth
+            val topHeight = Random.nextFloat() * (screenHeight / 2)
+            val bottomHeight = screenHeight - topHeight - pipeGapSize
+            val newPipePair = PipePair(
+                x = initialPipeX,
+                y = topHeight + pipeGapSize / 2,
+                topHeight = topHeight,
+                bottomHeight = bottomHeight
+            )
+            pipePairs.add(newPipePair)
+        }
+    }
+
 
     fun stopTheBee() {
         beeVelocity = 0f
